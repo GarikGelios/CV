11ebfb3db33dc9673574cfc1702e1709823639a5
GarikGelios <garikgelios@gmail.com>
2022-12-02 15:20:06 +0100

    Allow super-linter/stylelint have empty source

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
           // Translates CSS into CommonJS
           'css-loader',
           // Compiles Sass to CSS
-          'sass-loader',
+          'sass-loader'
         ]
       }
     ]
