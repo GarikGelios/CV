93f763425d513b377d35c2d84fdfa3026821b105
GarikGelios <garikgelios@gmail.com>
2022-12-05 11:38:56 +0100

    Add separate css bundle file in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 module.exports = {
-  entry: './dev-main.js',
+  entry: ['./assets/stylesheets/sass/main.scss', './assets/javascript/dev-main.js'], // everything will merge into a single main.js. If you want separate files  lets create an object. Read more at concepts/entry-points/
+  output: {
+    clean: true, // Clean the output directory before emit.
+  },
   mode: 'development',
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader'
-        ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: []
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        type: "asset/resource",
+        generator: {
+          filename: "bundle.css",
+        },
+        use: ["sass-loader"]
       }
     ]
   }
