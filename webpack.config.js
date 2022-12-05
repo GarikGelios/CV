62a2033856095883be0f1d505c93e20dfe6f565e
GarikGelios <garikgelios@gmail.com>
2022-12-05 12:35:27 +0100

    Fix js and add to ignore .css in dist/ folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: ['./assets/stylesheets/sass/main.scss', './assets/javascript/dev-main.js'], // everything will merge into a single main.js. If you want separate files  lets create an object. Read more at concepts/entry-points/
   output: {
-    clean: true, // Clean the output directory before emit.
+    clean: true // Clean the output directory before emit.
   },
   mode: 'development',
   module: {
@@ -14,11 +14,11 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "bundle.css",
+          filename: 'bundle.css'
         },
-        use: ["sass-loader"]
+        use: ['sass-loader']
       }
     ]
   }
