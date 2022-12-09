d44fb6265788c8126197609865215e895443859a
GarikGelios <garikgelios@gmail.com>
2022-12-09 12:36:39 +0100

    Install Bootstrap via NPM and import it in scss and js main dev files

--- assets/javascript/dev-main.js
+++ assets/javascript/dev-main.js
@@ -1,1 +1,5 @@
+// Import our custom CSS
 import '../stylesheets/sass/main.scss'
+
+// Import all of Bootstrap's JS
+import 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
           filename: 'bundle.css'
         },
         use: ['sass-loader']
+      },
+      {
+        mimetype: 'image/svg+xml',
+        scheme: 'data',
+        type: 'asset/resource',
+        generator: {
+          filename: 'icons/[hash].svg'
+        }
       }
     ]
   }
