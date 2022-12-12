6d8dbb191362b8f9a15432598f755e053276442a
GarikGelios <garikgelios@gmail.com>
2022-12-12 15:31:14 +0100

    Move portfolio description to the modal

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path')
 
 module.exports = {
   entry: './assets/javascript/dev-main.js', // everything will merge into a single main.js. If you want separate files  lets create an object. Read more at concepts/entry-points/
+  resolve: {
+    alias: {
+      './fonts/bootstrap-icons.woff': path.resolve(__dirname, './node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff'),
+      './fonts/bootstrap-icons.woff2': path.resolve(__dirname, './node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2')
+    }
+  },
   output: {
     clean: true, // Clean the output directory before emit.
     filename: 'main.js',
@@ -20,18 +26,17 @@ module.exports = {
         generator: {
           filename: 'bundle.css'
         },
-        use: ['style-loader', 'sass-loader']
+        use: ['sass-loader']
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // fonts not working
+        type: 'asset/inline'
       },
       {
-        mimetype: 'image/svg+xml',
-        scheme: 'data',
+        test: /\.(ico|png|jpg|jpeg|gif|svg|webp|tiff)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'icons/[hash].svg'
+          filename: 'images/[name].[hash][ext]'
         }
       }
     ]
