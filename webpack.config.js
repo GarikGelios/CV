3dbb150bd6124ef00b560f1397ac385acdadb45e
GarikGelios <garikgelios@gmail.com>
2022-12-14 15:21:15 +0100

    Get fonts in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './'), // leave the original index file in the root of the project and the path to it until you make an html build
+    port: 8080,
+    hot: true
+  },
+  mode: 'development', // so that the resulting files are not minified (alternative 'production')
   module: {
     rules: [
       {
@@ -21,16 +26,24 @@ module.exports = {
         use: []
       },
       {
-        test: /\.scss$/,
-        type: 'asset/resource',
+        test: /\.s[ac]ss$/i,
+        // type: 'asset/resource', // type creates a separate file, which makes it impossible to get the fonts
         generator: {
-          filename: 'bundle.css'
+          filename: 'bundle.css' // now all styles are in javascript, the file is not created
         },
-        use: ['sass-loader']
+        use: [
+          'style-loader', // Creates `style` nodes from JS strings
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader' // Compiles Sass to CSS
+        ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i, // fonts not working
-        type: 'asset/inline'
+        type: 'asset/resource',
+        generator: {
+          // outputPath: 'fonts/', // how to force styles to look in this folder?
+          filename: '[name].[ext]'
+        }
       },
       {
         test: /\.(ico|png|jpg|jpeg|gif|svg|webp|tiff)$/i,
